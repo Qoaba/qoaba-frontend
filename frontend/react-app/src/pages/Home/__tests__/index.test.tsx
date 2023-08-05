1429af081f300b75a8a87c793d7d67706b5f924a
Bhavanvir Rai <bhavanvir.r@gmail.com>
2023-08-04 22:04:39 -0700

    Deleting Vite project

--- frontend/react-app/src/pages/Home/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Home } from './../index.tsx';
-
-test('renders welcome text', () => {
-    render(<Home />);
-    const welcomeText = screen.getByText(/Welcome to Mantine!/i);
-    expect(welcomeText).toBeInTheDocument();
-    }
-);
