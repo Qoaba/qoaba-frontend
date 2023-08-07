eb7cec279bcad80343b042a64562a2a364be7002
alexwholland <alexwholland@gmail.com>
2023-08-06 20:06:45 -0700

    Changed default color scheme to purply purple

--- frontend/web-app/src/app/emotion.tsx
+++ frontend/web-app/src/app/emotion.tsx
@@ -19,7 +19,13 @@ export default function RootStyleRegistry({ children }: { children: React.ReactN
 
   return (
     <CacheProvider value={cache}>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MantineProvider withGlobalStyles withNormalizeCSS
+      theme = {{
+        colors: {
+          blue: ["#f3f0ff", "#e5dbff", "#d0bfff", "#b197fc", "#9775fa", "#845ef7", "#7950f2", "#7048e8", "#6741d9", "#5f3dc4",]
+        }
+      }}
+      >
         {children}
       </MantineProvider>
     </CacheProvider>
