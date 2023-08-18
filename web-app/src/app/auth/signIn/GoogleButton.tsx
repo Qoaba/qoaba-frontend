34b890375fc8d93c9c64c91d309e3cc99faa6157
kevin <kmathew@peloton-technologies.com>
2023-08-17 19:54:31 -0700

    initial google auth initialization

--- web-app/src/app/auth/signIn/GoogleButton.tsx
+++ web-app/src/app/auth/signIn/GoogleButton.tsx
@@ -1,6 +1,7 @@
 import { GoogleIcon } from "./GoogleIcon";
 import { Button, ButtonProps, Group } from "@mantine/core";
+import { signIn } from "next-auth/react"
 
 export function GoogleButton(props: ButtonProps) {
-  return <Button leftIcon={<GoogleIcon />} variant="filled" {...props} />;
+  return <Button onClick={() => signIn("google")} leftIcon={< GoogleIcon />} variant="filled" {...props} />;
 }
