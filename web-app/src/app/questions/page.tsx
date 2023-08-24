ef7496e879db4f3dbbca8aad79a4b461085ca2e5
alexwholland <alexwholland@gmail.com>
2023-08-23 17:50:29 -0700

    Connected api to frontend

--- web-app/src/app/questions/page.tsx
+++ web-app/src/app/questions/page.tsx
@@ -1,86 +1,98 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HeaderMegaMenu } from "./Header";
-import { TableSort } from "./Table";
-import { Grid } from "@mantine/core";
+import { Grid, Text } from "@mantine/core";
 import { ProgressCardColored } from "./ProgressCard";
 import { StatsRingCard } from "./StatsCard";
 import { CardsCarousel } from "./Carousel";
 import { FooterLinks } from "../components/Footer";
+import { TableSort } from "./Table";
 
-const data = [
-  {
-    topic: "OOP",
-    difficulty: "Beginner",
-    question: "OOP concepts",
-  },
-  {
-    topic: "Networks",
-    difficulty: "Intermediate",
-    question: "TCP/IP compairson",
-  },
-  {
-    topic: "Algorithms",
-    difficulty: "Advanced",
-    question: "linear programming implementations",
-  },
-];
-
-const test = [
-  {
-    title: "Project tasks",
-    completed: 1887,
-    total: 2334,
-    stats: [
-      {
-        value: 447,
-        label: "Remaining",
-      },
-      {
-        value: 76,
-        label: "In progress",
-      },
-    ],
-  },
-];
-
-const footerData = [
-  {
-    title: "Products",
-    links: [
-      { label: "Feature 1", link: "#" },
-      { label: "Feature 2", link: "#" },
-      // Add more links as needed
-    ],
-  },
-  {
-    title: "Resources",
-    links: [
-      { label: "Documentation", link: "#" },
-      { label: "Tutorials", link: "#" },
-      // Add more links as needed
-    ],
-  },
-  // Add more groups as needed
-];
+interface ApiDataItem {
+  id: string;
+  topic: string;
+  difficulty: string;
+  question_title: string;
+  question: string;
+  solution: string;
+}
 
 export default function Page() {
+  const [apiData, setApiData] = useState<ApiDataItem[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/questions") // Change the URL to your API endpoint
+      .then((response) => response.json())
+      .then((data) => setApiData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const tableData = apiData.map((item) => ({
+    topic: item.topic,
+    difficulty: item.difficulty,
+    question: item.question,
+    question_title: item.question_title,
+    solution: item.solution,
+  }));
+
+  const test = [
+    {
+      title: "Project tasks",
+      completed: 1887,
+      total: 2334,
+      stats: [
+        {
+          value: 447,
+          label: "Remaining",
+        },
+        {
+          value: 76,
+          label: "In progress",
+        },
+      ],
+    },
+  ];
+
+  const footerData = [
+    {
+      title: "Products",
+      links: [
+        { label: "Feature 1", link: "#" },
+        { label: "Feature 2", link: "#" },
+        // Add more links as needed
+      ],
+    },
+    {
+      title: "Resources",
+      links: [
+        { label: "Documentation", link: "#" },
+        { label: "Tutorials", link: "#" },
+        // Add more links as needed
+      ],
+    },
+    // Add more groups as needed
+  ];
+
   return (
     <div>
       <HeaderMegaMenu />
       <Grid gutterMd={0}>
         <Grid.Col span="auto"></Grid.Col>
         <Grid.Col span={5}>
-          <CardsCarousel /> {/* No padding here */}
+          <CardsCarousel />
           <div style={{ marginTop: "20px" }}>
-            <TableSort data={data} /> {/* No padding here */}
+            {tableData.length > 0 ? (
+              <TableSort data={tableData}></TableSort>
+            ) : (
+              <Text weight={500} align="center">
+                Nothing found
+              </Text>
+            )}
           </div>
         </Grid.Col>
         <Grid.Col span={2}>
           <div style={{ marginLeft: "20px", marginRight: "20px" }}>
-            {" "}
-            {/* Padding on left and right */}
             <StatsRingCard
               title={test[0].title}
               total={test[0].total}
@@ -95,8 +107,6 @@ export default function Page() {
               marginRight: "20px",
             }}
           >
-            {" "}
-            {/* Padding on left and right */}
             <ProgressCardColored />
           </div>
         </Grid.Col>
